b5ff989ef0218f03d64bc20126d80e08d5aa21bd
Sagar Patil <sagarreddypatil@gmail.com>
2020-09-19 12:40:11 -0400

    Wrap code in name main

--- main.py
+++ main.py
@@ -80,94 +80,95 @@ class ConvNet(nn.Module):
         return x
 
 
-is_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if is_cuda else "cpu")
+if __name__ == "__main__":
+    is_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if is_cuda else "cpu")
 
-kwargs = {"batch_size": params.batch_size}
-if is_cuda:
-    kwargs.update({"num_workers": 1, "pin_memory": True, "shuffle": True})
+    kwargs = {"batch_size": params.batch_size}
+    if is_cuda:
+        kwargs.update({"num_workers": 1, "pin_memory": True, "shuffle": True})
 
-transform = transforms.Compose([transforms.ToTensor()])
+    transform = transforms.Compose([transforms.ToTensor()])
 
-train_set = datasets.CIFAR10(
-    "../../data", train=True, download=True, transform=transform
-)
-test_set = datasets.CIFAR10("../../data", train=False, transform=transform)
+    train_set = datasets.CIFAR10(
+        "../../data", train=True, download=True, transform=transform
+    )
+    test_set = datasets.CIFAR10("../../data", train=False, transform=transform)
 
-train_loader = torch.utils.data.DataLoader(train_set, **kwargs)
-test_loader = torch.utils.data.DataLoader(test_set, **kwargs)
+    train_loader = torch.utils.data.DataLoader(train_set, **kwargs)
+    test_loader = torch.utils.data.DataLoader(test_set, **kwargs)
 
-label_names = [
-    "airplane",
-    "automobile",
-    "bird",
-    "cat",
-    "deer",
-    "dog",
-    "frog",
-    "horse",
-    "ship",
-    "truck",
-]
-metric_names = ["loss", "accuracy"]
+    label_names = [
+        "airplane",
+        "automobile",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    ]
+    metric_names = ["loss", "accuracy"]
 
-model = ConvNet(num_classes=10)
-model.to(device)
+    model = ConvNet(num_classes=10)
+    model.to(device)
 
-num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(f"Parameters: {num_params}")
+    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Parameters: {num_params}")
 
-optimizer = torch.optim.Adam(model.parameters(), lr=params.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=params.lr)
 
-writer = SummaryWriter(log_dir="./logs")
-temp_ipt = torch.rand(5, 3, 32, 32, device=device)
+    writer = SummaryWriter(log_dir="./logs")
+    temp_ipt = torch.rand(5, 3, 32, 32, device=device)
 
-writer.add_graph(model, temp_ipt)
+    writer.add_graph(model, temp_ipt)
 
-for epoch in range(params.epochs):
-    model.train()
+    for epoch in range(params.epochs):
+        model.train()
 
-    train_metrics = [0, 0]
+        train_metrics = [0, 0]
 
-    for idx, (data, target) in tqdm(
-        enumerate(train_loader), f"Epoch: {epoch + 1}", total=len(train_loader)
-    ):
-        data, target = data.to(device), target.to(device)
-
-        optimizer.zero_grad()
-        output = model(data)
-
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-
-        train_metrics[0] += loss.item()
-
-        output = output.cpu().detach().numpy()
-        target = target.cpu().detach().numpy()
-        output = np.argmax(output, 1)
-
-        train_metrics[1] += metrics.accuracy_score(y_true=target, y_pred=output)
-
-    train_metrics = np.array(train_metrics) / len(train_loader)
-    for name, val in zip(metric_names, train_metrics):
-        writer.add_scalar("train/" + name, val, epoch)
-
-    test_metrics = [0, 0]
-    model.eval()
-    with torch.no_grad():
-        for data, target in test_loader:
+        for idx, (data, target) in tqdm(
+            enumerate(train_loader), f"Epoch: {epoch + 1}", total=len(train_loader)
+        ):
             data, target = data.to(device), target.to(device)
+
+            optimizer.zero_grad()
             output = model(data)
 
-            test_metrics[0] += F.nll_loss(output, target).item()
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+
+            train_metrics[0] += loss.item()
 
             output = output.cpu().detach().numpy()
             target = target.cpu().detach().numpy()
             output = np.argmax(output, 1)
 
-            test_metrics[1] += metrics.accuracy_score(y_true=target, y_pred=output)
+            train_metrics[1] += metrics.accuracy_score(y_true=target, y_pred=output)
 
-    test_metrics = np.array(test_metrics) / len(test_loader)
-    for name, val in zip(metric_names, test_metrics):
-        writer.add_scalar("test/" + name, val, epoch)
+        train_metrics = np.array(train_metrics) / len(train_loader)
+        for name, val in zip(metric_names, train_metrics):
+            writer.add_scalar("train/" + name, val, epoch)
+
+        test_metrics = [0, 0]
+        model.eval()
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+
+                test_metrics[0] += F.nll_loss(output, target).item()
+
+                output = output.cpu().detach().numpy()
+                target = target.cpu().detach().numpy()
+                output = np.argmax(output, 1)
+
+                test_metrics[1] += metrics.accuracy_score(y_true=target, y_pred=output)
+
+        test_metrics = np.array(test_metrics) / len(test_loader)
+        for name, val in zip(metric_names, test_metrics):
+            writer.add_scalar("test/" + name, val, epoch)
